8a290a34aedc7b90b15bef0342e77e720acd58ea
filiprrs <filiprrs@gmail.com>
2017-07-30 22:01:59 +0200

    Revert "cypress: Add LED on keypress support"
    This reverts commit 9e73486748e2f6c8d945d092af207b52797974a1.

--- drivers/input/keyboard/cypress/cypress-touchkey.c
+++ drivers/input/keyboard/cypress/cypress-touchkey.c
@@ -141,7 +141,6 @@ MODULE_DEVICE_TABLE(i2c, sec_touchkey_id);
 extern int get_touchkey_firmware(char *version);
 static int touchkey_led_status;
 static int touchled_cmd_reversed;
-static int led_on_keypress = 0;
 
 static int touchkey_debug_count;
 static char touchkey_debug[104];
@@ -195,19 +194,6 @@ static ssize_t brightness_control(struct device *dev,
 	return size;
 }
 #endif
-
-static ssize_t led_on_keypress_read( struct device *dev, struct device_attribute *attr, char *buf )
-{
-	return sprintf(buf,"%d\n", led_on_keypress);
-}
-
-static ssize_t led_on_keypress_write( struct device *dev, struct device_attribute *attr, const char *buf, size_t size )
-{
-	if (!strncmp(buf, "on", 2)) led_on_keypress = 1;
-	else if (!strncmp(buf, "off", 3)) led_on_keypress = 0;
-	else sscanf(buf,"%d\n", &led_on_keypress);
-	return size;
-}
 
 static void set_touchkey_debug(char value)
 {
@@ -842,12 +828,6 @@ static irqreturn_t touchkey_interrupt(int irq, void *dev_id)
 		#endif
 	}
 	set_touchkey_debug('A');
-
-	if (led_on_keypress) {
-		printk(KERN_DEBUG "[TouchKey] pressed: Turn LED on\n");
-		touchkey_led_status = TK_CMD_LED_ON;
-		i2c_touchkey_write(tkey_i2c->client, (u8 *) &touchkey_led_status, 1);
-	}
 	return IRQ_HANDLED;
 }
 #else
@@ -1054,9 +1034,7 @@ static int sec_touchkey_late_resume(struct early_suspend *h)
 #endif
 
 #ifdef LED_LDO_WITH_REGULATOR
-	if (!led_on_keypress) {
-		change_touch_key_led_voltage(touchkey_voltage_brightness);
-	}
+	change_touch_key_led_voltage(touchkey_voltage_brightness);
 #endif
 	enable_irq(tkey_i2c->irq);
 
@@ -1237,12 +1215,12 @@ static ssize_t touchkey_led_control(struct device *dev,
 #else
 	data = ledCmd[data];
 #endif
-	if (!led_on_keypress || data == TK_CMD_LED_OFF) {
-		ret = i2c_touchkey_write(tkey_i2c->client, (u8 *) &data, 1);
 
-		if (ret == -ENODEV)
-			touchled_cmd_reversed = 1;
-	}
+	ret = i2c_touchkey_write(tkey_i2c->client, (u8 *) &data, 1);
+
+	if (ret == -ENODEV)
+		touchled_cmd_reversed = 1;
+
 	touchkey_led_status = data;
 
 	return size;
@@ -1630,7 +1608,6 @@ static DEVICE_ATTR(touchkey_firm_version_panel, S_IRUGO | S_IWUSR | S_IWGRP,
 static DEVICE_ATTR(touchkey_brightness, S_IRUGO | S_IWUSR | S_IWGRP, NULL,
 		   brightness_control);
 #endif
-static DEVICE_ATTR(touchkey_led_on_keypress, S_IRUGO | S_IWUGO, led_on_keypress_read, led_on_keypress_write);
 
 #if defined(CONFIG_TARGET_LOCALE_NAATT)
 static DEVICE_ATTR(touchkey_autocal_start, S_IRUGO | S_IWUSR | S_IWGRP, NULL,
@@ -1671,7 +1648,6 @@ static struct attribute *touchkey_attributes[] = {
 #ifdef LED_LDO_WITH_REGULATOR
 	&dev_attr_touchkey_brightness.attr,
 #endif
-	&dev_attr_touchkey_led_on_keypress.attr,
 #if defined(CONFIG_TARGET_LOCALE_NAATT)
 	&dev_attr_touchkey_autocal_start.attr,
 #endif
